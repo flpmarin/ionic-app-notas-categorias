19386dbd2071bd5e8a6b576a7df5b4b860c918b9
felipe_marin_st <flpmarin@digitechfp.com>
2024-11-15 12:58:11 +0100

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
-  </ion-app>
+
+  <ion-menu content-id="main-content">
+    <ion-header>
+      <ion-toolbar color="primary">
+        <ion-title>Aplicación Notas</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      AQUÍ VAN LOS ITEMS DEL MENÚ
+    </ion-content>
+  </ion-menu>
+
+  <ion-page id="main-content">
+    <ion-header>
+      <ion-toolbar color="primary">
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Aplicación Notas</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding"> 
+      <ion-router-outlet />
+    </ion-content>
+
+    <FooterBar/>
+  </ion-page>
+</ion-app>
 </template>
 
-<script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+<script lang="ts" setup>
+  import {IonApp, IonRouterOutlet , IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+  import FooterBar from '@/components/FooterBar.vue';
 </script>
+
+<style scoped>
+
+</style>
